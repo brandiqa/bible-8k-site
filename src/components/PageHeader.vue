e26b6fb8ab4479daf373520c0572fe8167d86aa8
Michael Wanyoike <micwanyoike@gmail.com>
2021-04-10 17:15:23 +0300

    Created page header

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="py-8 bg-gray-800">
+    <div class="container px-8 mx-auto mb-1 text-white">
+      <h1 class="text-4xl text-white font-bold-">{{ title }}</h1>
+      <p class="mt-2 text-lg">
+        {{ subtitle }}
+      </p>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String,
+    subtitle: String,
+  },
+})
+</script>
--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="container mx-auto px-8">
-    <h1 class="text-white font-bold- text-4xl">Season 1</h1>
-    <p>Episode list coming soon....</p>
+  <page-header title="Scheduled Releases" subtitle="List of episodes" />
+  <div class="container px-8 mx-auto my-12">
+    <h2 class="text-2xl font-bold">Season 1</h2>
+    <p>Episode list TBA...(to be announced)</p>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PageHeader from '../components/PageHeader.vue'
+
+export default defineComponent({
+  components: { PageHeader },
+  setup() {},
+})
+</script>
