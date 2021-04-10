e9a2c3515055d778c609767c14f29807dcaa4dcb
Michael Wanyoike <micwanyoike@gmail.com>
2021-04-10 16:53:54 +0300

    Implemented header style in contact

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="py-8 px-4 text-center text-sm text-gray-300">
+  <footer class="px-4 py-8 text-sm text-center text-gray-300 bg-gray-800">
     <span>All Rights Reserved. Copyright @ 2021</span>
   </footer>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="container mx-auto px-8">
-    <h1 class="text-white font-bold- text-4xl">Contact</h1>
+  <header class="py-8 bg-gray-800">
+    <div class="container px-8 mx-auto mb-1 text-white">
+      <h1 class="text-4xl text-white font-bold-">Contact</h1>
+      <p class="mt-2 text-lg">
+        React out if you would like to participate or you have any questions
+      </p>
+    </div>
+  </header>
+  <div class="container px-8 mx-auto my-12">
     <contact-form />
   </div>
 </template>
