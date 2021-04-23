95856df1f15e9c06b55efe114a20327723bc0d76
Michael Wanyoike <micwanyoike@gmail.com>
2021-04-23 15:52:10 +0300

    Finalized first draft for home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
         Hi! My name is <span class="text-cyan-400">Michael</span>.
       </h1>
       <p class="mt-2 text-lg">
-        Am working on producing
+        Am planning on producing
         <span class="text-lime-400">
           cinematically beautiful CG animated series
         </span>
@@ -19,22 +19,23 @@
       <h2 class="title">Why</h2>
       <p class="text-lg text-center text-white">
         Bible stories are fundamental to our growth as Christians. It is our
-        duty raise our future generations under
+        duty to raise our future generations under
         <span class="text-lime-400">God's Word</span>.
       </p>
       <p>
         Animation is a powerful story-telling medium that connects emotionally
         with audiences quite differently than live action films. Using modern
-        art skills, performance acting and technology, we can deliver an
-        immersive, stylized presentation of the world that was written about in
-        the Bible.
+        art skills, dramatized acting and the latest CG &amp; audio technology,
+        we can deliver an immersive, stylized presentation of events recorded in
+        the Bible that occurred many years ago.
       </p>
       <p>
         As 21st century artists, we have the ability to richly re-create ancient
-        worlds and to re-imagine historical figures in order to provide visual
-        context for the written scripture. Understanding the Bible requires time
-        and meditation. The works produced by this project will simply be a form
-        of Bible study for those wishing to delve further.
+        worlds and to re-imagine historical figures in order to provide a visual
+        and auditory context for the written scripture. Understanding the Bible
+        requires time, deliberation and meditation. The works produced by this
+        project will simply be a study resource for anyone wishing to learn
+        about Christianity.
       </p>
     </div>
 
@@ -43,8 +44,8 @@
     <div class="py-8 alt">
       <div class="content">
         <h2 class="title">Plan</h2>
-        <p class="text-gray-900 subtitle">
-          The <span class="font-bold">first step</span> is to
+        <p class="text-lg text-gray-900">
+          <span class="font-bold">STEP 1:</span> We need to
           <span class="font-bold">find volunteers</span> who would be interested
           in
           <span class="font-bold">
@@ -58,42 +59,23 @@
           </span>
           this project and making it a <span class="font-bold">reality</span>.
         </p>
-        <div class="ml-8 text-gray-800">
-          <p>
-            We need to start filling the following roles with experienced
-            personnel:
-          </p>
-          <ul class="mt-2 list-disc">
-            <li>Historians</li>
-            <li>Animation Director</li>
-            <li>Film Editor</li>
-            <li>Story/Script writers</li>
-            <li>Concept artists</li>
-          </ul>
-        </div>
         <p class="text-lg text-gray-900">
-          The <span class="font-bold">second step</span> is to start early
-          <span class="font-bold">pre-production</span>, and come up with an
-          <span class="font-bold">initial budget</span>.
+          <span class="font-bold">STEP 2:</span> Develop an
+          <span class="font-bold">animation pipeline</span> suitable for
+          <span class="font-bold">remote production teams</span> by working on
+          <span class="font-bold">animated short films</span>.
         </p>
-        <div class="ml-8 text-gray-800">
-          <p>
-            We need to select the first Bible story to animate, allocate a
-            production time-frame and develop the following assets which
-            include:
-          </p>
-          <ul class="my-2 list-disc">
-            <li>
-              Animation budget for pre-production, production, post-production
-              &amp; distribution
-            </li>
-            <li>Script, Storyboard and Animatics</li>
-            <li>Film Look Development</li>
-          </ul>
-        </div>
+        <p class="ml-4 text-gray-700 font=semibold">
+          By working on multiple animated shorts, the newly formed team will be
+          able level up their skills in preparation for achieving high
+          production values when developing and producing the animated Bible
+          series.
+        </p>
         <p class="text-lg text-gray-900">
-          The <span class="font-bold">third step</span> is to raise
-          <span class="font-bold">funding</span> which has been outlined below.
+          <span class="font-bold">STEP 3:</span> Build an
+          <span class="font-bold">online community of fans</span> that would
+          benefit from watching the upcoming
+          <span class="font-bold">animated Bible series</span> for young adults.
         </p>
       </div>
     </div>
@@ -105,41 +87,35 @@
         <span class="font-bold text-lime-400">crowdfunding</span>.
       </p>
       <p>
-        A campaign will be launched to grow interest and build an online
-        community of members who are interested in watching a
-        <span class="font-bold">polished animated Bible story</span> developed
-        for young adults.
+        For film makers, crowdfunding is probably the best way to raise funds
+        that guarantees freedom of creativity without interference from
+        investors and institutions.
       </p>
       <p>
-        With the help of additional volunteers, we can develop a
-        <span class="font-bold"> 90 second trailer</span> to help popularize the
-        project.
+        By building and engaging with a community early, we hope to deliver
+        content that speaks to the minds and hearts of our audiences.
       </p>
       <p>
-        Once we have a big enough community and our initial target budget has
-        been raised, we can hire additional artists, actors, composers and other
-        stakeholders required to develop, produce and distribute our first
-        animated Bible story.
+        Plans for crowdfunding will be revealed once all
+        <span class="italic font-semibold text-white">3 STEPS in the PLAN</span>
+        have been attained.
       </p>
     </div>
   </div>
 
   <hr />
 
-  <div class="px-4 py-6 text-center bg-teal-400">
-    <div class="mb-2 text-2xl font-bold text-gray-900">
-      Want to Participate?
-    </div>
-    <div class="container mx-auto text-lg text-gray-800">
-      <p>
-        If you would like to get involved in the project or just have a
-        conversation, you can send me an email directly by clicking the button
-        below.
+  <div class="py-8 alt">
+    <div class="text-center content">
+      <h2 class="title">Want to Participate?</h2>
+      <p class="text-xl text-gray-900">
+        If you would like to get involved in this project in any way, you can
+        send me an email directly by clicking the button below.
       </p>
       <button
         class="mt-8 bg-gray-900 btn text-lime-400 hover:bg-lime-500 hover:text-gray-900"
       >
-        <router-link to="/contact">Reach Out</router-link>
+        <router-link to="/contact">Connect with Michael</router-link>
       </button>
     </div>
   </div>
