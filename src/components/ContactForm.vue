c9994d5a3f98c4ec0f77db434039b9291af32bd2
Michael Wanyoike <micwanyoike@gmail.com>
2021-04-24 23:32:44 +0300

    Converted forms to composition api

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <Form class="max-w-screen-sm" @submit="onSubmit" :validation-schema="schema">
+  <form class="max-w-screen-sm" @submit="onSubmit">
     <Alert v-if="errorMsg" type="error" title="Error" :message="errorMsg" />
     <Alert
       v-if="successMsg"
@@ -9,28 +9,27 @@
     />
     <div class="field">
       <label for="name">Name</label>
-      <Field name="name" type="text" />
-      <ErrorMessage class="error" name="name" />
+      <input type="text" name="name" v-model="name" />
+      <span class="error">{{ nameError }} </span>
     </div>
 
     <div class="field">
       <label for="email">Email</label>
-      <Field name="email" type="email" />
-      <ErrorMessage class="error" name="email" />
+      <input type="email" name="email" v-model="email" />
+      <span class="error">{{ emailError }} </span>
     </div>
 
     <div class="field">
       <label for="message">Message</label>
-      <text-area-input name="message" rows="5" />
-      <ErrorMessage class="error" name="message" />
+      <textarea name="message" :rows="5" v-model="message" />
+      <span class="error">{{ messageError }} </span>
     </div>
 
     <button
       class="mt-2 text-gray-900 btn bg-cyan-400 hover:bg-lime-400"
-      :class="loading && 'bg-lime-600 text-lime-50 cursor-not-allowed'"
-      :disabled="loading"
+      :disabled="isSubmitting"
     >
-      <span class="inline-flex items-center" v-if="loading">
+      <span class="inline-flex items-center" v-if="isSubmitting">
         <svg
           class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
           xmlns="http://www.w3.org/2000/svg"
@@ -56,25 +55,20 @@
       <span class="animate-pulse" v-else-if="errorMsg">Try again</span>
       <span v-else>Send Message</span>
     </button>
-  </Form>
+  </form>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { Form, Field, ErrorMessage, FormActions } from 'vee-validate'
+import { useForm, useField } from 'vee-validate'
 import { object, string } from 'yup'
 import axios from 'axios'
 
 import Alert from './Alert.vue'
-import TextAreaInput from './TextAreaInput.vue'
 
 export default defineComponent({
   name: 'ContactForm',
   components: {
-    Form,
-    Field,
-    ErrorMessage,
-    TextAreaInput,
     Alert,
   },
   setup() {
@@ -88,49 +82,60 @@ export default defineComponent({
         .min(5, 'Message must be at least 5 characters'),
     })
 
-    return {
-      schema,
-      loading: ref(false),
-      successMsg: ref(''),
-      errorMsg: ref(''),
-    }
-  },
-  methods: {
-    async onSubmit(values: Object, { resetForm }: FormActions<any>) {
+    const { handleSubmit, isSubmitting, resetForm } = useForm({
+      validationSchema: schema,
+    })
+
+    const { value: name, errorMessage: nameError } = useField('name')
+    const { value: email, errorMessage: emailError } = useField('email')
+    const { value: message, errorMessage: messageError } = useField('message')
+
+    const successMsg = ref('')
+    const errorMsg = ref('')
+
+    const onSubmit = handleSubmit(async (values) => {
       try {
         // start loading spinner
-        this.loading = true
-        this.errorMsg = ''
+        errorMsg.value = ''
         const endpoint = import.meta.env.VITE_USEBASIN_ENDPOINT as string
         const response = await axios.post(endpoint, values)
         if (response.status === 200) {
-          // reset form
           resetForm()
-          // show success message
-          this.successMsg = "Thanks for reaching out. I'll be in touch soon🙂"
-          this.loading = false
+          successMsg.value = "Thanks for reaching out. I'll be in touch soon🙂"
         }
       } catch (error) {
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          this.errorMsg =
+          errorMsg.value =
             'The request was made and the server responded with a status code' +
             error.response.status +
             error.response.data
           console.log(error.response.data)
         } else if (error.request) {
           // The request was made but no response was received
-          this.errorMsg = 'The request was made but no response was received'
+          errorMsg.value = 'The request was made but no response was received'
           // console.log(error.request)
         } else {
           // Something happened in setting up the request that triggered an Error
-          this.errorMsg = error.message
+          errorMsg.value = error.message
           console.log('Error', error.message)
         }
-        this.loading = false
       }
-    },
+    })
+
+    return {
+      onSubmit,
+      isSubmitting,
+      name,
+      nameError,
+      email,
+      emailError,
+      message,
+      messageError,
+      successMsg,
+      errorMsg,
+    }
   },
 })
 </script>
